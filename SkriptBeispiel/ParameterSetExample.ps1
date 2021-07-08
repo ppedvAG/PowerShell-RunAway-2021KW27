46ca9078a64c334b492be207e30e28548f84bee1
Stefan Ober <StefanO@ppedv.de>
2021-07-08 16:34:06 +0200

    ParameterSetExample hinzugefügt

---
+++ SkriptBeispiel/ParameterSetExample.ps1
@@ -0,0 +1,9 @@
+﻿[cmdletBinding(DefaulParameterSetName="Set1")]
+param(
+[Parameter(Mandatory=$true, ParameterSetName="Set1")]
+[Parameter(Mandatory=$false, ParameterSetName="Set2")]
+[switch]$Param1,
+
+[Parameter(Mandatory=$true, ParameterSetName="Set2")]
+[switch]$Param2
+)
